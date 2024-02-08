5dd58c1f2b60aa511b3e6a36ee7023140acb96fc
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-08 22:37:04 +0600

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,7 +57,7 @@ const Navbar = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            themoviedb
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
