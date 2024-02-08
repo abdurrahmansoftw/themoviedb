2a1a8420d29773805947611cc312b5e473a3c067
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-08 23:54:37 +0600

    add parent key

--- src/layouts/MovieGrid.tsx
+++ src/layouts/MovieGrid.tsx
@@ -15,9 +15,9 @@ const MovieGrid = () => {
       <Grid container spacing={2}>
         {isLoading &&
           skeletons.map((skeleton) => (
-            <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+            <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={skeleton}>
               <MovieCardContainer>
-                <MovieCardSkeleton key={skeleton} />
+                <MovieCardSkeleton />
               </MovieCardContainer>
             </Grid>
           ))}
