cad2fceec9abc2ddf1b3b18296868715aea94b8a
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-08 22:18:57 +0600

    useMovies

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,3 +1,47 @@
-const useMovies = () => {}
+import { useEffect, useState } from 'react'
+import { Movie } from '../layouts/MovieGrid'
+import apiClient from '../services/apiClient'
+
+interface Genre {
+  genre_ids: number
+}
+
+export interface Movie {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: Genre[]
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+interface FetchMovieRespose {
+  page: number
+  results: Movie[]
+  total_pages: number
+  total_results: number
+}
+
+const useMovies = () => {
+  const [movies, setMovies] = useState<Movie[]>([])
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    apiClient
+      .get<FetchMovieRespose>('/popular')
+      .then((res) => setMovies(res.data.results))
+      .catch((err) => setError(err.message))
+  }, [])
+
+  return { movies, error }
+}
 
 export default useMovies
--- src/layouts/MovieGrid.tsx
+++ src/layouts/MovieGrid.tsx
@@ -1,47 +1,10 @@
 import { Box, Grid } from '@mui/material'
-import { useEffect, useState } from 'react'
 import MovieCard from '../components/MovieCard'
-import apiClient from '../services/apiClient'
-
-interface Genre {
-  genre_ids: number
-}
-
-export interface Movie {
-  adult: boolean
-  backdrop_path: string
-  genre_ids: Genre[]
-  id: number
-  original_language: string
-  original_title: string
-  overview: string
-  popularity: number
-  poster_path: string
-  release_date: string
-  title: string
-  video: boolean
-  vote_average: number
-  vote_count: number
-}
-
-interface FetchMovieRespose {
-  page: number
-  results: Movie[]
-  total_pages: number
-  total_results: number
-}
+import useMovies from '../hooks/useMovies'
 
 const MovieGrid = () => {
-  const [movies, setMovies] = useState<Movie[]>([])
-  const [error, setError] = useState('')
-
-  useEffect(() => {
-    apiClient
-      .get<FetchMovieRespose>('/popular')
-      .then((res) => setMovies(res.data.results))
-      .catch((err) => setError(err.message))
-  }, [])
-
+  const { movies, error } = useMovies()
+  
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
