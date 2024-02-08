f1041c516c3ada477a0f17d55bce2404e7a1590b
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-08 22:35:56 +0600

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Container } from '@mui/material'
+import Navbar from './components/Navbar'
 import MovieGrid from './layouts/MovieGrid'
 
 const App = () => {
   return (
-    <Container>
-      <MovieGrid />
-    </Container>
+    <div>
+      <Navbar />
+
+      <Container className='my-5 '>
+        <MovieGrid />
+      </Container>
+    </div>
   )
 }
 
